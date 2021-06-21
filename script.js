73fd6cb1a5cfefd8875561a2b8aef4e32c3cd371
Quinn Lonergan <qui99lo@gmail.com>
2021-06-21 15:33:05 -0400

    Some JS added

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ const h2 = document.createElement('h2');
 h2.textContent = "Quinn Lonergan";
 
 document.querySelector('body').appendChild(h2); 
+
+
+function addingEventListener() {
+    const country = document.getElementById('country');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+country.addEventListener('click', clickAlert); 
+}
+
+addingEventListener();
+
+//function changeImage() {
+    //if (document.getElementById("countryimg").src == "images/51Qmd5JHNeL._AC_.jpg") 
+    //{
+    //document.getElementById("countryimg").src = "images/country2.jpg";
+    //}
+    //else 
+    //{
+    //document.getElementById("countryimg").src = "images/51Qmd5JHNeL._AC_.jpg";
+//}
+//}
+
+
